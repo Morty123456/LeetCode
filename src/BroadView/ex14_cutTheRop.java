31be99511e0e7504552638aebd7d583fd223cdd7
wzh <13021151355@163.com>
2020-02-23 16:53:03 +0800

    贪心算法，类似于找数学规律

--- src/BroadView/ex14_cutTheRop.java
+++ src/BroadView/ex14_cutTheRop.java
@@ -3,7 +3,10 @@ package BroadView;
 //见leetcode解析
 public class ex14_cutTheRop {
     public static void main(String[] args){
-        int m = 8;
+        System.out.println(tanxin(90));
+    }
+//    dp
+    public static int dp(int m){
         int[] cut = new int[m+1];
         cut[2]=2;
         cut[3]=3;
@@ -12,6 +15,27 @@ public class ex14_cutTheRop {
                 cut[i] = Math.max(cut[i], cut[j]*cut[i-j]);
             }
         }
-        System.out.println(cut[m]);
+        return cut[m];
+    }
+    public static int tanxin(int n){
+        if(n<=3) return n-1;
+        int a=n/3;
+        int b=n%3;
+        long rep;
+        if(b==0)
+            rep = power(3,a);
+        else if(b==1)
+            rep = power(3,a-1)*4 % 1000000007;
+        else
+            rep =  power(3,a)*2 % 1000000007;
+        return (int)rep;
+    }
+    public static long power(int a, int b){
+        long sum=1;
+        while(b-->0){
+            sum = sum * a % 1000000007;
+        }
+//        System.out.println("sum  ："+sum);
+        return sum;
     }
 }
