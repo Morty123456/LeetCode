56b31c9347aa50394c4aafba7a5762ef77e44cf6
wzh <13021151355@163.com>
2020-02-26 08:26:53 +0800

    之字形遍历二叉树：利用两个栈 层次遍历二叉树：利用一个队列

--- src/BroadView/ex32_printBinaryTreeFromTopToBottom.java
+++ src/BroadView/ex32_printBinaryTreeFromTopToBottom.java
@@ -3,6 +3,7 @@ package BroadView;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Stack;
 
 public class ex32_printBinaryTreeFromTopToBottom {
     public static void main(String[] args){
@@ -11,14 +12,16 @@ public class ex32_printBinaryTreeFromTopToBottom {
         root.right = new TreeNode(20);
         root.right.left = new TreeNode(15);
         root.right.right = new TreeNode(7);
-        List<List<Integer>> list = levelOrder(root);
+//        List<List<Integer>> list = levelOrder(root);
+        List<List<Integer>> list = zhiOrder(root);
         for (List<Integer> l : list){
             for (int a : l){
-                System.out.print(a+" ");
+                System.out.print(a+"； ");
             }
             System.out.println("");
         }
     }
+    //分层打印
     public static List<List<Integer>> levelOrder(TreeNode root){
         if (root==null)
             return new LinkedList<>();
@@ -50,5 +53,45 @@ public class ex32_printBinaryTreeFromTopToBottom {
         }
         arrList.add(list);
         return arrList;
+    }
+    //之字形打印
+    public static List<List<Integer>> zhiOrder(TreeNode root){
+        List<List<Integer>> arrlist = new LinkedList<>();
+        List<Integer> list = new LinkedList<>();
+        Stack<TreeNode> stack1 = new Stack<>();
+        Stack<TreeNode> stack2 = new Stack<>();
+        int level = 1;
+        stack1.push(root);
+        while (!stack1.isEmpty() || !stack2.isEmpty()){
+            if(level%2==1){
+                while (!stack1.isEmpty()){
+                    TreeNode node = stack1.pop();
+                    list.add(node.val);
+                    if(node.left!=null){
+                        stack2.push(node.left);
+                    }
+                    if (node.right!=null){
+                        stack2.push(node.right);
+                    }
+                }
+                arrlist.add(list);
+                list = new LinkedList<>();
+                level++;
+            }
+            else {
+                while (!stack2.isEmpty()){
+                    TreeNode node = stack2.pop();
+                    list.add(node.val);
+                    if (node.right!=null)
+                        stack1.add(node.right);
+                    if (node.left!=null)
+                        stack1.add(node.left);
+                }
+                arrlist.add(list);
+                list = new LinkedList<>();
+                level++;
+            }
+        }
+        return arrlist;
     }
 }
