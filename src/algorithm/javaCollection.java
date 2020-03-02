c794bab9a813c5e90bdf30c9a906a1143a1eeb20
wzh <13021151355@163.com>
2020-03-02 21:37:43 +0800

    Collection集合

---
+++ src/algorithm/javaCollection.java
@@ -0,0 +1,62 @@
+package algorithm;
+
+import java.util.*;
+
+//集合类的用法
+public class javaCollection {
+    public static void main(String[] args){
+//        listExample();
+//        setExample();
+        sollectionExample();
+    }
+    public static void sollectionExample(){
+        HashMap<Integer,Integer> hashMap = new HashMap<>();
+        hashMap.put(1,1);
+        hashMap.put(1,2);
+        Iterator<Integer> it = hashMap.keySet().iterator();
+        while (it.hasNext())
+            System.out.println(it.next()+" ");
+        System.out.println(hashMap.containsKey(1));
+    }
+    public static void setExample(){
+        Set<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(1);
+        set.add(2);
+        for (int a:set)
+            System.out.print(a+" ");
+        System.out.println();
+        set.remove(set.size()-1);
+        for (int a:set)
+            System.out.print(a+" ");
+        System.out.println();
+        Set<Integer> treeSet = new TreeSet<>();
+        treeSet.add(1);
+        treeSet.add(1);
+        treeSet.add(2);
+        for (int a:treeSet)
+            System.out.print(a+" ");
+        System.out.println();
+        System.out.println(treeSet.contains(2));
+    }
+    public static void listExample(){
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        List<Integer> list = new LinkedList<>();
+        arrayList.add(1);
+        arrayList.add(2);
+        for (int a: arrayList)
+            System.out.print(a);
+        System.out.println();
+        arrayList.remove(arrayList.size()-1);
+        for (int a: arrayList)
+            System.out.print(a);
+        System.out.println();
+
+        linkedList.add(1);
+        linkedList.add(2);
+        for (int a: linkedList)
+            System.out.print(a);
+        System.out.println();
+    }
+}
