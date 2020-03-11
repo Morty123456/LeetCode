831ebeb5e4a38009506810f5a12c6c16148a336b
wzh <13021151355@163.com>
2020-03-11 17:03:51 +0800

    HasMap计算容量的方法

---
+++ src/algorithm/HashMap.java
@@ -0,0 +1,21 @@
+package algorithm;
+
+public class HashMap {
+    public static void main(String[] args){
+        HashMap hashmap = new HashMap();
+        int cap1 = 2;
+        int cap2 = 7;
+        int cap3 = 0;
+        System.out.println(tableSizeFor(cap3));
+//        System.out.println(tableSizeFor(cap2));
+    }
+    public static final int tableSizeFor(int cap){
+        int n = cap - 1;
+        n |= n>>>1;
+        n |= n>>>2;
+        n |= n>>>4;
+        n |= n>>>8;
+        n |= n>>>16;
+        return (n < 0) ? 1 : (n >= (1<<30) ) ? (1<<30) : n+1;
+    }
+}
