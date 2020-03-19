faf7151b391f75d604de2d99259b0f326c9367d5
wzh <13021151355@163.com>
2020-03-19 18:06:22 +0800

    ++i,i++

---
+++ src/BroadView/ex1.java
@@ -0,0 +1,11 @@
+package BroadView;
+
+public class ex1 {
+    public static void main(String[] args){
+        int i = 1;
+        i = (i++);
+//        i = ++i;
+        System.out.println(i);
+    }
+}
+
