a27aece3b11d9d28a9651028df2d7f6e914aeb84
wzh <13021151355@163.com>
2020-03-23 11:07:56 +0800

    java小数计算精度损失

--- src/BroadView/ex1.java
+++ src/BroadView/ex1.java
@@ -2,10 +2,14 @@ package BroadView;
 
 public class ex1 {
     public static void main(String[] args){
-        int i = 1;
-        i = (i++);
-//        i = ++i;
-        System.out.println(i);
+//        int i = 1;
+//        i = (i++);
+////        i = ++i;
+//        System.out.println(i);
+        if (3*0.1 == 0.3)
+            System.out.println("true");
+        else
+            System.out.println("false");
     }
 }
 
