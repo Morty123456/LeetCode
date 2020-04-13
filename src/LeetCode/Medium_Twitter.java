f9de117f5bcff49ce564cde636c8352420925164
wzh <13021151355@163.com>
2020-04-13 21:34:52 +0800

    设计推特，有点产品的意思

---
+++ src/LeetCode/Medium_Twitter.java
@@ -0,0 +1,121 @@
+
+package LeetCode;
+import java.util.*;
+
+public class Medium_Twitter {
+
+
+    private Map<Integer, Tweet> twitter;
+
+    private Map<Integer, Set<Integer>> followings;
+
+    private static int timestamp = 0;
+
+    private static PriorityQueue<Tweet> maxHeap;
+
+    public Medium_Twitter() {
+        followings = new HashMap<>();
+        twitter = new HashMap<>();
+        maxHeap = new PriorityQueue<>((o1, o2) -> -o1.timestamp + o2.timestamp);
+    }
+
+    public void postTweet(int userId, int tweetId) {
+        timestamp++;
+        if (twitter.containsKey(userId)) {
+            Tweet oldHead = twitter.get(userId);
+            Tweet newHead = new Tweet(tweetId, timestamp);
+            newHead.next = oldHead;
+            twitter.put(userId, newHead);
+        } else {
+            twitter.put(userId, new Tweet(tweetId, timestamp));
+        }
+    }
+
+    public List<Integer> getNewsFeed(int userId) {
+        maxHeap.clear();
+
+        if (twitter.containsKey(userId)) {
+            maxHeap.offer(twitter.get(userId));
+        }
+
+        Set<Integer> followingList = followings.get(userId);
+        if (followingList != null && followingList.size() > 0) {
+            for (Integer followingId : followingList) {
+                Tweet tweet = twitter.get(followingId);
+                if (tweet != null) {
+                    maxHeap.offer(tweet);
+                }
+            }
+        }
+
+        List<Integer> res = new ArrayList<>(10);
+        int count = 0;
+        while (!maxHeap.isEmpty() && count < 10) {
+            Tweet head = maxHeap.poll();
+            res.add(head.id);
+
+            if (head.next != null) {
+                maxHeap.offer(head.next);
+            }
+            count++;
+        }
+        return res;
+    }
+
+    public void follow(int followerId, int followeeId) {
+        if (followeeId == followerId) {
+            return;
+        }
+        Set<Integer> followingList = followings.get(followerId);
+        if (followingList == null) {
+            Set<Integer> init = new HashSet<>();
+            init.add(followeeId);
+            followings.put(followerId, init);
+        } else {
+            if (followingList.contains(followeeId)) {
+                return;
+            }
+            followingList.add(followeeId);
+        }
+    }
+
+    public void unfollow(int followerId, int followeeId) {
+        if (followeeId == followerId) {
+            return;
+        }
+        Set<Integer> followingList = followings.get(followerId);
+
+        if (followingList == null) {
+            return;
+        }
+        followingList.remove(followeeId);
+    }
+    private class Tweet {
+        private int id;
+        private int timestamp;
+        private Tweet next;
+
+        public Tweet(int id, int timestamp) {
+            this.id = id;
+            this.timestamp = timestamp;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Medium_Twitter twitter = new Medium_Twitter();
+        twitter.postTweet(1, 1);
+        List<Integer> res1 = twitter.getNewsFeed(1);
+        System.out.println(res1);
+
+        twitter.follow(2, 1);
+
+        List<Integer> res2 = twitter.getNewsFeed(2);
+        System.out.println(res2);
+
+        twitter.unfollow(2, 1);
+
+        List<Integer> res3 = twitter.getNewsFeed(2);
+        System.out.println(res3);
+    }
+}
