3f465dcb31eb11cdcd7c5f34d5d25f8c49a6417c
wzh <13021151355@163.com>
2020-04-22 10:33:20 +0800

    计算是否能被3整除

---
+++ src/NowCoder/wangyi_divid3.java
@@ -0,0 +1,14 @@
+package NowCoder;
+
+import java.util.Scanner;
+
+public class wangyi_divid3 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int begin = sc.nextInt();
+        int end = sc.nextInt();
+        int count = 0;
+        count = (end-begin+1) - (end+2)/3 + (begin+1)/3;
+        System.out.println(count);
+    }
+}
