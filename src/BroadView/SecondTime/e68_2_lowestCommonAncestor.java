570de84689fbe0de491470cf825346822fdfeb5c
wzh <13021151355@163.com>
2020-06-18 22:23:56 +0800

    二叉树最近公共祖先

---
+++ src/BroadView/SecondTime/e68_2_lowestCommonAncestor.java
@@ -0,0 +1,18 @@
+package BroadView.SecondTime;
+
+public class e68_2_lowestCommonAncestor {
+    public static void main(String[] args){
+
+    }
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q){
+        if (root==null || root==p || root==q)
+            return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left==null)
+            return right;
+        if (right==null)
+            return left;
+        return root;
+    }
+}
