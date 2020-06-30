05c78d716ca07b438b8e9a53b2874d5bb99ca659
wzh <13021151355@163.com>
2020-06-30 09:38:15 +0800

    double浮点数计算不精确

---
+++ src/JVM/Num.java
@@ -0,0 +1,15 @@
+package JVM;
+
+/**
+ * @author: wzh
+ * @time: 2020/6/30 9:34
+ * @description:
+ */
+public class Num {
+    public static void main(String[] args){
+        double a = 0.1, b = 0.2;
+        System.out.println(a+" "+b);
+        double c = a+b;
+        System.out.println(c);
+    }
+}
