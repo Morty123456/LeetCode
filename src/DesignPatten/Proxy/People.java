1ef0468bd513d6e8780c4a1283c2e59e82cad5c4
wzh <13021151355@163.com>
2020-07-09 10:30:22 +0800

    jdk动态代理为什么需要接口

---
+++ src/DesignPatten/Proxy/People.java
@@ -0,0 +1,11 @@
+package DesignPatten.Proxy;
+
+/**
+ * @author: wzh
+ * @time: 2020/7/8 21:32
+ * @description:
+ */
+public interface People {
+    public String work();
+    public void say();
+}
