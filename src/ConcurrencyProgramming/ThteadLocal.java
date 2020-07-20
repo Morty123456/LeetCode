c8392cd6221ff13a7212221ca0eaf5189f2c284d
wzh <13021151355@163.com>
2020-07-21 07:46:35 +0800

    threadlocal

--- src/ConcurrencyProgramming/ThteadLocal.java
+++ src/ConcurrencyProgramming/ThteadLocal.java
@@ -3,14 +3,17 @@ package ConcurrencyProgramming;
 public class ThteadLocal {
     public static void main(String[] args){
         Thread t = new Thread();
-        ThreadLocal threadLocal = new ThreadLocal();
+        ThreadLocal<Integer> threadLocal = new ThreadLocal<>();
+        ThreadLocal<Integer> b = new ThreadLocal<>();
         threadLocal.set(1);
-        threadLocal.set(2);
+        b.set(1);
+        System.out.println(threadLocal.get()+" "+b.get());
         System.out.println(threadLocal.get());
         new Thread(()->{
-            threadLocal.set(1);
-            System.out.println(threadLocal.get());
-        }).start();
+            threadLocal.set(2);
 
+            System.out.println(threadLocal.get()+" "+b.get());
+        }).start();
+        System.out.println("aaa  "+threadLocal.get());
     }
 }
