6cd96373ddca19c9d913aae4b35baa5dbc1cdd89
Morty123456 <13021151355@163.com>
2020-08-11 20:56:14 +0800

    condition

--- src/ConcurrencyProgramming/ConditionTest.java
+++ src/ConcurrencyProgramming/ConditionTest.java
@@ -42,7 +42,65 @@ class BoundQueue<T> {
     }
 
     public void add(T t) throws InterruptedException {
-//        lock.lock();
+        lock.lock();
+        try {
+            while (count == items.length){
+                System.out.println("队列已满");
+//                notFull.await();
+                wait();
+            }
+            items[addIndex] = t;
+            System.out.println(addIndex+" 位置开始入队");
+            if (++addIndex == items.length){
+                addIndex = 0;
+//                System.out.println("开始新的一轮");
+            }
+            ++count;
+            System.out.println("队列共有 "+count+" 个值");
+//            notEmpty.signal();
+            notify();
+        }finally {
+            lock.unlock();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public T remove() throws InterruptedException{
+        lock.lock();
+        try {
+            while (count==0){
+                System.out.println("队列是空的");
+//                notEmpty.await();
+                wait();
+            }
+            Object x = items[removeIndex];
+            System.out.println(removeIndex+" 位置开始出队");
+            if (++removeIndex == items.length)
+                removeIndex = 0;
+            --count;
+            System.out.println("队列共有 "+count+" 个值");
+//            notFull.signal();
+            notify();
+            return (T)x;
+        }finally {
+            lock.unlock();
+        }
+    }
+}
+
+class BoundQueueSyn<T> {
+    private Object[] items;
+    private int addIndex, removeIndex, count;
+    private Lock lock = new ReentrantLock();
+    private Condition notEmpty = lock.newCondition();
+    private Condition notFull = lock.newCondition();
+//    private Object object;
+
+    public BoundQueueSyn(int size) {
+        items = new Object[size];
+    }
+
+    public void add(T t) throws InterruptedException {
         synchronized (this){
             try {
                 while (count == items.length){
@@ -60,36 +118,34 @@ class BoundQueue<T> {
                 System.out.println("队列共有 "+count+" 个值");
 //            notEmpty.signal();
                 notify();
-            }finally {
-//                lock.unlock();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-
         }
+
     }
 
     @SuppressWarnings("unchecked")
     public T remove() throws InterruptedException{
-//        lock.lock();
         synchronized (this){
             try {
-                while (count==0){
+                while (count == 0) {
                     System.out.println("队列是空的");
 //                notEmpty.await();
                     wait();
                 }
                 Object x = items[removeIndex];
-                System.out.println(removeIndex+" 位置开始出队");
+                System.out.println(removeIndex + " 位置开始出队");
                 if (++removeIndex == items.length)
                     removeIndex = 0;
                 --count;
-                System.out.println("队列共有 "+count+" 个值");
+                System.out.println("队列共有 " + count + " 个值");
 //            notFull.signal();
                 notify();
-                return (T)x;
-            }finally {
-//                lock.unlock();
+                return (T) x;
+            } finally {
             }
         }
-
     }
 }
+
